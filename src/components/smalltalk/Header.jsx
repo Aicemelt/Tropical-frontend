7d2e46c23d9fc38ed36460a6f7e1d8e63cef7a97
dohee <doheeeq@gmail.com>
2025-09-11 16:38:50 +0900

    header 컴포넌트 생성

--- src/components/smalltalk/Header.jsx
+++ src/components/smalltalk/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Header.module.scss';
 
-const {logo} = styles;
+const {logo, title, userName, desc} = styles;
 
 const Header = () => {
     return (
         <div>
             <h1 className={logo}></h1>
-            <h2>안녕하세요, 트로피칼님</h2>
-            <p>햄보르기니님의 캘린터, 일기, 버킷리스트, 투두리스트 기반으로 스몰토크 주제를 추천드리겠습니다.</p>
+            <h2 className={title}>안녕하세요, <span className={userName}>트로피칼</span>님</h2>
+            <p className={desc}>햄보르기니님의 캘린터, 일기, 버킷리스트, 투두리스트 기반으로 스몰토크 주제를 추천드리겠습니다.</p>
         </div>
     );
 };
