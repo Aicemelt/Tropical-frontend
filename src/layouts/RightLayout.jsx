7287a9ccbb302c264bbc0933597a4980c98d9320
dohee-jin <doheeeq@gmail.com>
2025-09-13 02:08:14 +0900

    filter tab 컴포넌트 생성

--- src/layouts/RightLayout.jsx
+++ src/layouts/RightLayout.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {rightLayout} from '../styles/layouts/Layout.module.scss';
 import {Outlet, Route} from "react-router-dom";
 import RightHeader from "../components/common/RightHeader.jsx";
+import CalendarPage from "../pages/CalendarPage.jsx";
+import TodoPage from "../pages/TodoPage.jsx";
 
 const RightLayout = () => {
     return (
         <div className={rightLayout}>
             <RightHeader />
+            <CalendarPage />
+            <TodoPage />
         </div>
     );
 };
---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoFilter from "../components/todo/TodoFilter.jsx";
+
+const TodoPage = () => {
+    return (
+        <>
+            <TodoFilter />
+        </>
+    );
+};
+
+export default TodoPage;
