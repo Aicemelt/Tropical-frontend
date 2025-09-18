633f98748d08a2140d72db80661eb6e716847fb8
Sirosho <pack8447@gmail.com>
2025-09-18 14:34:54 +0900

    헤더 텍스트 동적 변경을 위한 상태 변수 및 함수 추가

--- src/components/common/RightHeader.jsx
+++ src/components/common/RightHeader.jsx
@@ -11,23 +11,39 @@ const RightHeader = () => {
         setIsBlock(!isBlock);
     }
 
+    const handleHeaderChange = (name) => {
+        setHeaderName(name); // 헤더 이름 상태 업데이트
+        setIsBlock(false); // 메뉴 닫기
+    }
+
+    const[headerName, setHeaderName] =useState("Calender");
+
     return (
         <header>
             <h3>
             {/*  헤더 텍스트는 동적으로 변경  */}
-                Calender
+                {headerName}
             </h3>
             <button className={`${styles.dropdownBtn}`} type={"button"} onClick={handleMenu}></button>
             <nav style={isBlock ? {display: "block"} : {display: "none"}}>
                 <ul>
                     <li>
-                        <NavLink to={'/'} className={({isActive}) => isActive ? `${styles.navLink} ${styles.on}` : styles.navLink}>Calender</NavLink>
+                        <NavLink to={'/'}
+                                 className={({isActive}) => isActive ? `${styles.navLink} ${styles.on}` : styles.navLink}
+                                 onClick={() => handleHeaderChange('Calender')}
+                        >Calender</NavLink>
                     </li>
                     <li>
-                        <NavLink to={'/todo'} className={({isActive}) => isActive ? `${styles.navLink} ${styles.on}` : styles.navLink}>Todo</NavLink>
+                        <NavLink to={'/todo'}
+                                 className={({isActive}) => isActive ? `${styles.navLink} ${styles.on}` : styles.navLink}
+                                 onClick={() => handleHeaderChange('Todo')}
+                        >Todo</NavLink>
                     </li>
                     <li>
-                        <NavLink to={'/bucket'} className={({isActive}) => isActive ? `${styles.navLink} ${styles.on}` : styles.navLink}>Bucket List</NavLink>
+                        <NavLink to={'/bucket'}
+                                 className={({isActive}) => isActive ? `${styles.navLink} ${styles.on}` : styles.navLink}
+                                 onClick={() => handleHeaderChange('Bucket List')}
+                        >Bucket List</NavLink>
                     </li>
                 </ul>
             </nav>
